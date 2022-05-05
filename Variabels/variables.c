fb6b5fd19cccb53fbd9c4b6034cdb4e66ba9ec29
Mahmoud Helmy <77947371+MaMH20@users.noreply.github.com>
2022-05-05 12:31:04 +0200

    Variables in C

---
+++ Variabels/variables.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+
+	int x; //declaration
+	x = 123; //intialization
+
+	int y = 321; //declaration + intialization
+
+	int age = 23;
+	float gpa = 2.05;
+	char grade = 'C';
+	char name[] = "LOL"; //array of characters //C has no String!!
+
+	printf("Hello %s\n", name);
+	printf("you are %d years old\n", age);
+	printf("your grade is %c\n", grade);
+	printf("your gpa is %f\n", gpa);
+
+
+	
+	
+	
+
+
+	return 0;
+
+
+}
