4a4e1871105fc3fe508bc12f5661b4e2c4ba04a4
Mahmoud Helmy <77947371+MaMH20@users.noreply.github.com>
2022-05-06 17:35:56 +0200

    Math Functions in C

---
+++ Math/math.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double a = sqrt(9);
+	double b = pow(2,5);
+	int c = round(5.2);  //round a number 
+	int d = ceil(5.2);   //round a number Up = 6
+	int e = floor(2.7); // round a number Down = 2
+	double f = fabs(-100);
+	double g = log(3);
+	double h = sin(45);
+	double i = cos(45);
+	double j = tan(45);
+
+
+	printf("%0.2lf\n", a);
+	printf("%0.2lf\n", b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+	printf("%d\n",e);
+	printf("%lf\n",f);
+	printf("%lf\n", g);
+	printf("%lf\n", h);
+	printf("%lf\n",i);
+	printf("%lf\n", j);
+
+
+
+
+
+}
