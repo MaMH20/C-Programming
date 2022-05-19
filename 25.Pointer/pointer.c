96520602a75eb02c3246cbff2065cfbbdb3e2142
Mahmoud Helmy <77947371+MaMH20@users.noreply.github.com>
2022-05-19 13:24:08 +0200

    Pointer in C

--- 25.Pointer/pointer.c
+++ 25.Pointer/pointer.c
@@ -1,13 +1,58 @@
 #include<stdio.h>
 
+//void printAge(int age);
+
+void printAge(int +pAge);
+
+int main() {
+
+
+	// pointer = a "variable-like" reference that holds a memory address to another variable, array, etc.
+	//           some tasks are performed more easily with pointers
+	//           * = indirection operator (value at address)
+	//
 
 
 
-in main() {
+	int age = 25;
+	//int *pAge = &age;   //the same data type of the variable which we pointed to !!
+	int *pAge = NULL; //good practice to assign NULL if declring a pointer
+	pAge = &age;
+	printf("Address of age: %p\n", &age);
+	printf("Value of pAge:  %p\n", pAge);
 
 
-return 0;
+	printf("Value of age: %d\n", age);
+	printf("Value at Stored address: %d\n", *pAge); //derefrencing 
+
+	printf("Size of age %d bytes: \n", sizeof(age));
+	printf("Sze of pAge %d bytes: \n", sizeof(pAge)); 
+
+
+
+
+	//printAge(age);
+
+
+	printf(pAge);
+	return 0;
 
 
 }
 
+
+
+
+/*void printAge(int age) {
+
+
+  printf("you are %d yers old", age);
+
+  }*/
+
+void printAge(int *pAge) {
+
+
+		printf("you are %d yers old", *pAge); //dereferce
+
+		}
